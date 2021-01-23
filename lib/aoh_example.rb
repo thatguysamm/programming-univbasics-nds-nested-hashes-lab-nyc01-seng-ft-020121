f3e28ec08ada21daba429f666252f5117ab6a944
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-23 17:24:46 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,7 +17,13 @@ def assembled_aoh
 end
 
 def literal_aoh
-  literal_aoh= result
+  literal_aoh=  [
+    DON_G,
+    JOELLE_VD,
+    PAT_M,
+    KATE_G,
+    BRUCE_G
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
